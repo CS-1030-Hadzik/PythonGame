fc55b49335f44ce89fb28505f9aea1a4da7f4f18
Scott Hadzik <scotthadzik@weber.edu>
2021-02-10 08:52:43 -0700

    User Selection initial capabilities

--- game.py
+++ game.py
@@ -2,3 +2,19 @@
 # Feb 10, 2021
 
 # This is a game to demo Python Functions
+
+#Rock Paper Scissors
+
+# Rock beats Scissors
+# Scissors beats Paper
+# Paper beats Rock
+
+# Pseudo Code --> 
+
+# User Selection -- get info from user
+# Computer Selection -- find way for computer to select options
+# Evaluate the results
+# Declare a winner
+
+user_selection = input('What is your selection? (Rock, Paper, Scissors')
+print ('You chose', user_selection)
