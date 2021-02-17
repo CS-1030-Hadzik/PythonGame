7169c7042c8c53f4b4bc622f88fe7cc1b7b137fc
Scott Hadzik <scotthadzik@weber.edu>
2021-02-17 08:36:46 -0700

    Added conditional statements to evaluate winner

--- game.py
+++ game.py
@@ -9,10 +9,44 @@ user_selection = input('What is your selection? (Rock, Paper, Scissors): ')
 print ('You chose', user_selection)
 
 # Computer Selection -- find way for computer to select options
-computer_selection = random.choices(possible_choices)
+# computer_selection = random.choices(possible_choices) # TODO uncomment for production
+
+#TODO remove this temp test
+computer_selection = "Paper"
+
 print ('Computer chose', str(computer_selection))
 
+# Evaluate the results -- Conditional statement 
+if user_selection == computer_selection:
+    print('It is a tie')
 
-# Evaluate the results
+#------Rock-----
+elif user_selection == 'Rock':
+    # computer selects Paper
+    if computer_selection == 'Paper':
+        print('User loses')
+    # computer selects Scissors
+    else:
+        print('User wins')
+
+#------Paper-----
+elif user_selection == 'Paper':
+    # computer selects Rock
+    if computer_selection == 'Rock':
+        print('User wins')
+    # computer selects Scissors
+    else:
+        print ('User loses')
+    
+      
+#------Scissors-----      
+elif user_selection == 'Scissors':
+    # computer selects Rock
+    if computer_selection == 'Rock':
+        print ('User loses')
+    # computer selects Paper
+    else:
+        print ('User wins')
+
 # Declare a winner
 
